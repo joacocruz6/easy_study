cefb857414883ccf2cf83a5003a6685191249e09
Joaquin Cruz <joacocruz6@gmail.com>
2022-06-07 18:53:48 -0400

    feat(authentication): create authenticated and non authenticated apps

--- src/App.js
+++ src/App.js
@@ -11,42 +11,50 @@ import LoginPage from "./components/LoginPage/LoginPage";
 import Profile from "./components/Profile/Profile";
 import ChangePasswordForm from "./components/ChangePasswordForm/ChangePasswordForm";
 import CreateAccountForm from "./components/CreateAccount/CreateAccount";
-const Main = () => {
+import AnonymousLanding from "./components/AnonymousLanding/AnonymousLanding";
+
+const Main = (props) => {
+	const isAuthenticated = props.isAuthenticated;
+	let routes = (
+		<>
+			<Route path="/" element={<AnonymousLanding />}></Route>
+			<Route path="account/create" element={<CreateAccountForm />} />
+			<Route path="login" element={<LoginPage />} />
+		</>
+	);
+	if (isAuthenticated) {
+		routes = (
+			<>
+				<Route path="/" element={<HomePage />} />
+				<Route path="exercises">
+					<Route path="create" element={<CreateForm />} />
+					<Route path="all" element={<ExerciseList />} />
+					<Route path=":exercise_uuid" element={<ExerciseDetail />} />
+					<Route path="my_exercises" element={<ExerciseList />} />
+				</Route>
+				<Route path="profile" element={<Profile />} />
+				<Route
+					path="change_password"
+					element={<ChangePasswordForm />}
+				/>
+			</>
+		);
+	}
 	return (
 		<div className="main">
 			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="exercises">
-						<Route path="create" element={<CreateForm />} />
-						<Route path="all" element={<ExerciseList />} />
-						<Route
-							path=":exercise_uuid"
-							element={<ExerciseDetail />}
-						/>
-						<Route path="my_exercises" element={<ExerciseList />} />
-					</Route>
-					<Route path="login" element={<LoginPage />} />
-					<Route path="profile" element={<Profile />} />
-					<Route
-						path="change_password"
-						element={<ChangePasswordForm />}
-					/>
-					<Route
-						path="account/create"
-						element={<CreateAccountForm />}
-					/>
-				</Routes>
+				<Routes>{routes}</Routes>
 			</BrowserRouter>
 		</div>
 	);
 };
 
 function App() {
+	const isAuthenticated = false;
 	return (
 		<div className="App">
-			<Navigation />
-			<Main />
+			<Navigation isAuthenticated={isAuthenticated} />
+			<Main isAuthenticated={isAuthenticated} />
 		</div>
 	);
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,12 +3,38 @@ import Container from "react-bootstrap/Container";
 import "./Navbar.css";
 import NavigationMenu from "./NavigationMenu/NavigationMenu.jsx";
 import NavigationLogout from "./NavigationMenu/NavigationLogout/NavigationLogout.jsx";
-const Navigation = () => {
+import NavigationLogIn from "./NavigationMenu/NavigationLogIn/NavigationLogIn";
+import NavigationRegister from "./NavigationMenu/NavigationRegister/NavigationRegister";
+
+const NavbarProfile = (props) => {
+	const isAuthenticated = props.isAuthenticated;
+	if (isAuthenticated) {
+		return (
+			<>
+				<Navbar.Text>
+					Signed in as: <span className="user_text">Mark Otto</span>
+				</Navbar.Text>
+				<NavigationLogout />
+			</>
+		);
+	}
+	return (
+		<>
+			<NavigationRegister />
+			<NavigationLogIn />
+		</>
+	);
+};
+
+const Navigation = (props) => {
+	const isAuthenticated = props.isAuthenticated;
+	const menu = isAuthenticated ? <NavigationMenu /> : "";
+
 	return (
 		<>
 			<Navbar className="navigation" fixed="top" variant="dark">
 				<Container>
-					<NavigationMenu />
+					{menu}
 					<Navbar.Toggle />
 					<Navbar.Collapse style={{ "padding-left": "27vw" }}>
 						<Navbar.Brand href="/">
@@ -16,11 +42,7 @@ const Navigation = () => {
 						</Navbar.Brand>
 					</Navbar.Collapse>
 					<Navbar.Collapse className="justify-content-end">
-						<Navbar.Text>
-							Signed in as:{" "}
-							<span className="user_text">Mark Otto</span>
-						</Navbar.Text>
-						<NavigationLogout />
+						<NavbarProfile isAuthenticated={isAuthenticated} />
 					</Navbar.Collapse>
 				</Container>
 			</Navbar>
