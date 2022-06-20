f9d6f76c39fd31c8f8c5ef8ec97fa56c86c2bd92
Joaquin Cruz <joacocruz6@gmail.com>
2022-06-20 18:05:49 -0400

    feat(profile): connect profile with backend

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,48 +1,145 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
 import CardForm from "../CardForm/CardForm";
 import Button from "react-bootstrap/Button";
 import { Form } from "react-bootstrap";
+import Placeholder from "react-bootstrap/Placeholder";
+import requests from "../../utils/requests";
+import handleInput from "../../utils/handleInput";
 
 const Profile = () => {
-	const title = "Mark Otto Profile";
+	const [isLoading, setIsLoading] = useState(true);
+	const [userEmail, setUserEmail] = useState("");
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
 
-	const button = (
+	useEffect(() => {
+		const config = {
+			headers: {
+				Authorization: `Token ${localStorage.getItem("token")}`,
+			},
+		};
+		const endpoint = "/api/v100/user/current/";
+		requests
+			.get(endpoint, config)
+			.then((response) => response.json())
+			.then((data) => {
+				setUserEmail(data["email"]);
+				setFirstName(data["first_name"]);
+				setLastName(data["last_name"]);
+				setIsLoading(false);
+			});
+	}, []);
+
+	const handleFormSubmit = (event) => {
+		event.preventDefault();
+		const endpoint = "/api/v100/user/update/";
+		const data = {
+			first_name: firstName,
+			last_name: lastName,
+		};
+		console.log(data);
+		const config = {
+			method: "PUT",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Token ${localStorage.getItem("token")}`,
+			},
+			body: JSON.stringify(data),
+		};
+		requests
+			.request(endpoint, config)
+			.then((response) => response.json())
+			.then((data) => console.log(data));
+	};
+
+	const title = isLoading ? (
+		<>
+			<Placeholder xs={4} />
+		</>
+	) : (
+		`${firstName} ${lastName} Profile`
+	);
+
+	const button = isLoading ? (
+		<>
+			<Placeholder.Button variant="success" xs={4} />
+		</>
+	) : (
 		<Button type="submit" variant="success">
 			Save
 		</Button>
 	);
-	const email = (
+	const emailValue = isLoading ? (
+		<>
+			<br />
+			<Placeholder xs={6} />
+		</>
+	) : (
+		<Form.Control plaintext readOnly defaultValue={userEmail} />
+	);
+	const emailInput = (
 		<>
 			<Form.Group className="mb-3" controlId="formEmail">
-				<Form.Label>Email:</Form.Label>
-				<Form.Control
-					plaintext
-					readOnly
-					defaultValue="mark.otto@gmail.com"
-				/>
+				<Form.Label>Email</Form.Label>
+				{emailValue}
 			</Form.Group>
 		</>
+	);
+	const firstNameValue = isLoading ? (
+		<>
+			<br />
+			<Placeholder xs={6} />
+		</>
+	) : (
+		<Form.Control
+			type="input"
+			defaultValue={firstName}
+			onChange={handleInput(setFirstName)}
+		/>
 	);
 	const firstNameInput = (
 		<>
 			<Form.Group className="mb-3" controlId="formFirstName">
 				<Form.Label>First Name</Form.Label>
-				<Form.Control type="input" defaultValue="Mark" />
+				{firstNameValue}
 			</Form.Group>
 		</>
 	);
+	const lastNameValue = isLoading ? (
+		<>
+			<br />
+			<Placeholder xs={6} />
+		</>
+	) : (
+		<Form.Control
+			type="input"
+			defaultValue={lastName}
+			onChange={handleInput(setLastName)}
+		/>
+	);
+
 	const lastNameInput = (
 		<>
 			<Form.Group className="mb-3" controlId="formLastName">
 				<Form.Label>Last Name</Form.Label>
-				<Form.Control type="input" defaultValue="Otto" />
+				{lastNameValue}
 			</Form.Group>
 		</>
 	);
-	const changePassword = (
+	const changePassword = isLoading ? (
+		<>
+			<Placeholder.Button
+				variant="secondary"
+				xs={4}
+				style={{ marginTop: "10px" }}
+			/>
+		</>
+	) : (
 		<Button
 			href="/change_password"
-			style={{ margin: "10px" }}
+			style={{ marginTop: "10px" }}
 			variant="secondary"
 		>
 			Change Password
@@ -52,9 +149,10 @@ const Profile = () => {
 	return (
 		<CardForm
 			cardTitle={title}
-			inputs={[email, firstNameInput, lastNameInput]}
+			inputs={[emailInput, firstNameInput, lastNameInput]}
 			saveButton={button}
 			extraComponents={[changePassword]}
+			onSubmit={handleFormSubmit}
 		/>
 	);
 };
