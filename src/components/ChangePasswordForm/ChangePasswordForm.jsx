11a269aa316116688353f32bd89ca52640db760c
Joaquin Cruz <joacocruz6@gmail.com>
2022-06-23 18:59:58 -0400

    feat(change_password): integrate the password creation

--- src/components/ChangePasswordForm/ChangePasswordForm.jsx
+++ src/components/ChangePasswordForm/ChangePasswordForm.jsx
@@ -1,44 +1,91 @@
 import React from "react";
+import { useState } from "react";
 import Form from "react-bootstrap/Form";
 import CardForm from "../CardForm/CardForm";
 import Button from "react-bootstrap/Button";
-
+import requests from "../../utils/requests";
+import handleInput from "../../utils/handleInput";
 const ChangePasswordForm = () => {
+	const [oldPassword, setOldPassword] = useState("");
+	const [newPassword, setNewPassword] = useState("");
+	const [confirmNewPassword, setConfirmNewPassword] = useState("");
+
+	const submit = (event) => {
+		event.preventDefault();
+		const url = "/api/v100/user/change_password/";
+		const config = {
+			mode: "cors",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Token ${localStorage.getItem("token")}`,
+			},
+		};
+		const data = {
+			current_password: oldPassword,
+			new_password: newPassword,
+			confirm_new_password: confirmNewPassword,
+		};
+		console.log(data);
+		requests
+			.post(url, config, data)
+			.then((response) => {
+				if (response.ok) {
+					return response.json();
+				}
+				throw new Error("Something went wrong");
+			})
+			.then((data) => (document.location = "/profile/"))
+			.catch((error) => console.log(error));
+	};
 	const title = "Change Password";
 	const submitButton = (
 		<Button type="submit" variant="success">
 			Change
 		</Button>
 	);
-	const oldPassword = (
+	const oldPasswordInput = (
 		<>
 			<Form.Group className="mb-3" controlId="formOldPassword">
 				<Form.Label>Old Password</Form.Label>
-				<Form.Control type="password" />
+				<Form.Control
+					type="password"
+					onChange={handleInput(setOldPassword)}
+				/>
 			</Form.Group>
 		</>
 	);
-	const newPassword = (
+	const newPasswordInput = (
 		<>
 			<Form.Group className="mb-3" controlId="formNewPassword">
 				<Form.Label>New Password</Form.Label>
-				<Form.Control type="password" />
+				<Form.Control
+					type="password"
+					onChange={handleInput(setNewPassword)}
+				/>
 			</Form.Group>
 		</>
 	);
-	const confirmNewPassword = (
+	const confirmNewPasswordInput = (
 		<>
 			<Form.Group className="mb-3" controlId="formConfirmNewPassword">
 				<Form.Label>Confirm New Password</Form.Label>
-				<Form.Control type="password" />
+				<Form.Control
+					type="password"
+					onChange={handleInput(setConfirmNewPassword)}
+				/>
 			</Form.Group>
 		</>
 	);
 	return (
 		<CardForm
 			cardTitle={title}
-			inputs={[oldPassword, newPassword, confirmNewPassword]}
+			inputs={[
+				oldPasswordInput,
+				newPasswordInput,
+				confirmNewPasswordInput,
+			]}
 			saveButton={submitButton}
+			onSubmit={submit}
 		/>
 	);
 };
